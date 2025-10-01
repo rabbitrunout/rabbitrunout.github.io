2610c1acdefe7b08931f7bf478b1e8b662e3723a
Irina Saf <irina.safronova0801@gmail.com>
2025-09-30 21:22:16 -0400

    Updated mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -45,20 +45,21 @@ export const Projects = () => {
   },);
 
   const sliderSettings = {
-    dots: true,
-    infinite: false,
-    speed: 600,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    swipe: true,
-    swipeToSlide: true,
-    touchMove: true,
-    arrows: true,
-    responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 2 } },
-      { breakpoint: 768, settings: { slidesToShow: 1 } },
-    ],
-  };
+  dots: true,
+  infinite: false,
+  speed: 600,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  swipe: true,
+  swipeToSlide: true,
+  touchMove: true,
+  arrows: true,
+  responsive: [
+    { breakpoint: 1200, settings: { slidesToShow: 2 } }, // планшет
+    { breakpoint: 768, settings: { slidesToShow: 1, centerMode: true, centerPadding: "20px" } }, // мобилка
+  ],
+};
+
 
   const categories = ["All", ...Object.keys(projectsByCategory)];
 
