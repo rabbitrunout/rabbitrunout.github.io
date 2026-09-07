bc97478e464ee2fe52aadec43922b2639424ecc4
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2026-09-07 12:27:16 -0400

    Avoid duplicate education and preserve section URLs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+import React from "react";
 import "./App.css";
 
 import { NavBar } from "./components/NavBar";
@@ -9,36 +9,11 @@ import Experience from "./components/Experience";
 import FeaturedProjects from "./components/FeaturedProjects";
 import HorizontalProjects from "./components/HorizontalProjects";
 import Skills from "./components/Skills";
-import Education from "./components/Education";
 import Certificates from "./components/Certificates";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-  useLayoutEffect(() => {
-    if ("scrollRestoration" in window.history) {
-      window.history.scrollRestoration = "manual";
-    }
-
-    if (window.location.hash) {
-      window.history.replaceState(
-        null,
-        "",
-        window.location.pathname + window.location.search
-      );
-    }
-
-    window.scrollTo(0, 0);
-
-    requestAnimationFrame(() => {
-      window.scrollTo(0, 0);
-    });
-
-    setTimeout(() => {
-      window.scrollTo(0, 0);
-    }, 0);
-  }, []);
-
   return (
     <div className="app editorial-app">
       <NavBar />
@@ -49,7 +24,6 @@ function App() {
       <AISection />
       <HorizontalProjects />
       <Skills />
-      <Education />
       <Certificates />
       <Contact />
       <Footer />
